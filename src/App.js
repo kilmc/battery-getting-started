c0cb37ebe55f852dd6695550df63ee749d078e2b
Kilian McMahon <kil@kilmc.com>
2018-09-29 15:42:14 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="sans-serif bg-black h100vh flex">
+      <h2 className="inline-block white m-auto fz7vw">Battery + React + MDX</h2>
+    </div>
+  );
+};
+
+export default App;
